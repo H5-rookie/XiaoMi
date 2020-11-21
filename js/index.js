c97ee1c5f049e201b3e0d66d570b697bde3a3f65
zzg <525617324@qq.com>
2020-11-21 11:41:22 +0800

    开始了

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-document.querySelector('h1').onclick = function (){
-  alert(123);
-};
+
+
+var resGoods = document.getElementById("res_goods");
+resGoods.onclick =function(){
+
+}
+/* 触摸大导航出现下拉框 缺少运动 */
+$(function(){
+  $("#down_goods").css("display","none");
+  $(".nav_lis>a").hover(function(){
+    $("#down_goods").css("display","block");
+  },function(){
+    $("#down_goods").css("display","none");
+  });
+});
+
+/* 轮播图 */
+var box_swiper = document.querySelector('.home_Pic_container');
+var swiper = document.querySelector('.home_Pic');
+var swiper_images = document.querySelector('.home_Pic');
+var imgLiS = slider.children
+console.log(swiper_images);
